7d547efecf4dbf9f168d216cb1c71293db20f1cf
Jeaninne <jeaninnel@hotmail.com>
2020-05-31 21:40:33 -0400

    Final changes. everything works

--- app.js
+++ app.js
@@ -4,32 +4,102 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​
+// ​const OUTPUT_DIR = path.resolve(__dirname, "output")
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const render = require("./lib/htmlRenderer");
+const employeeArray = [];
+let id = 1;
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+function init() {
+    inquirer.prompt([
+        {
+            message: "Which type of team member would you like to add?",
+            name: "employeeType",
+            type: "list",
+            choices: ["Manager", "Engineer", "Intern", "Quit"]
+        }
+    ]).then(function (answer) {
+        if (answer.employeeType === "Manager") {
+            addManager();
+        } else if (answer.employeeType === "Engineer") {
+            addEngineer();
+        } else if (answer.employeeType === "Intern") {
+            addIntern();
+        } else {
+            writeHTML();
+        }
+    });
+};
+
+function addManager() {
+    inquirer.prompt([
+        {
+            message: "What is your manager's name?",
+            name: "name",
+            type: "input"
+        }, {
+            message: "What is your manager's email address?",
+            name: "email",
+            type: "input"
+        }, {
+            message: "What is your manager's office number?",
+            name: "officeNum",
+            type: "input"
+        },
+
+    ]).then(answer => {
+        const manager = new Manager(answer.name, id++, answer.email, answer.officeNum);
+        employeeArray.push(manager);
+        init();
+    })
+}
+function addEngineer() {
+    inquirer.prompt([
+        {
+            message: "What's your engineer's name?",
+            name: "name",
+            type: "input"
+        }, {
+            message: "What's your engineer's email?",
+            name: "email",
+            type: "input"
+        }, {
+            message: "What's your engineer's GitHub?",
+            name: "gitHub",
+            type: "input"
+        },
+    ]).then(answer => {
+        const engineer = new Engineer(answer.name, id++, answer.email, answer.gitHub);
+        employeeArray.push(engineer);
+        init();
+    })
+}
+function addIntern() {
+    inquirer.prompt([
+        {
+            message: "What's your intern's name?",
+            name: "name",
+            type: "input"
+        }, {
+            message: "What's your intern's email?",
+            name: "email",
+            type: "input"
+        }, {
+            message: "What's your intern's school?",
+            name: "school",
+            type: "input"
+        },
+    ]).then(answer => {
+        const intern = new Intern(answer.name, id++, answer.email, answer.school);
+        employeeArray.push(intern);
+        init();
+    })
+}
+function writeHTML() {
+    console.log(employeeArray);
+
+}
+
+init();
+
